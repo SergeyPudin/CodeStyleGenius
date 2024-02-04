78001efa46142ff7ef2faad797b990d07792ef45
SergeyPudin <121240126+SergeyPudin@users.noreply.github.com>
2024-02-04 16:32:57 +0300

    Add Shooter and Bullet

---
+++ Shooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private float _bulletSpeed;
+    [SerializeField] private Bullet _bulletPrefab;
+    [SerializeField] private Transform _objectToShoot;
+    [SerializeField] float _timeBetweenShots;
+
+    private void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    private IEnumerator Shoot()
+    {
+        while (true)
+        {
+            Rigidbody bulletRigidbody;
+            Vector3 direction = (_objectToShoot.position - transform.position).normalized;
+
+            Bullet newBullet = Instantiate(_bulletPrefab, transform.position + direction, Quaternion.identity);
+
+            if (newBullet.TryGetComponent(out bulletRigidbody))
+            {
+                bulletRigidbody.transform.up = direction;
+                bulletRigidbody.velocity = direction * _bulletSpeed;
+            }
+
+            yield return new WaitForSeconds(_timeBetweenShots);
+        }
+    }
+}
